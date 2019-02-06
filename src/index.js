52b442e8f613b8245307b45248c9b31a7c801963
Dominik Huber <dhuber666@gmail.com>
2019-02-06 17:23:52 +0100

    connected react to redux store and set up folder structure

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
+import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import store from './config/store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 if (module.hot) {
   module.hot.accept();
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+
+// TODO: Add a real reducer
+export default combineReducers({
+  recipes: (state = {}) => state
+});
